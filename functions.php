0fe0b2d656897b27c46c50ec94cea037543cc495
Mostafa Abdelrahman <mab@amp.co>
2018-11-09 15:27:39 +0000

    Create script files

---
+++ functions.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * Returns true only if $string contains $contains
+ *
+ * @param $string
+ * @param $contains
+ * @return bool
+ */
+function str_contains($string, $contains)
+{
+    return strpos($string, $contains) !== false;
+}
+
+/**
+ * Returns true only if $string starts with $startsWith
+ *
+ * @param $string
+ * @param $startsWith
+ * @return bool
+ */
+function str_starts_with($string, $startsWith)
+{
+    return substr($string, 0, strlen($startsWith)) === $startsWith;
+}
+
+/**
+ * Returns true only if $string ends with $endsWith
+ *
+ * @param $string
+ * @param $endsWith
+ * @return bool
+ */
+function str_ends_with($string, $endsWith)
+{
+    return strlen($endsWith) == 0 || substr($string, -strlen($endsWith)) === $endsWith;
+}
---
+++ src/PatchFile.php
@@ -0,0 +1,61 @@
+<?php
+
+class PatchFile
+{
+    /** @var string */
+    private $path;
+
+    /** @var SplFileObject */
+    private $file;
+
+    /**
+     * @param string $path
+     */
+    public function __construct($path)
+    {
+        $this->path = $path;
+        $this->reset();
+    }
+
+    /**
+     * @return string
+     */
+    public function getPath()
+    {
+        return $this->path;
+    }
+
+    /**
+     * Returns a list of the files affected by this patch
+     *
+     * @return array
+     */
+    public function getFiles()
+    {
+        $files = [];
+        $this->file->rewind();
+
+        while (!$this->file->eof()) {
+            $line = $this->file->fgets();
+            if (str_starts_with($line, 'diff -ur ')) {
+                $parts = explode(' ', $line);
+                $files[] = $parts[3];
+            }
+        }
+
+        return $files;
+    }
+
+    /**
+     * Resets the file, should be called after modifying the file
+     */
+    private function reset()
+    {
+        if (file_exists($this->path)) {
+            $this->file = new SplFileObject($this->path);
+            $this->file->setFlags(SplFileObject::DROP_NEW_LINE);
+        } else {
+            throw new Exception($this->path . ' does not exist');
+        }
+    }
+}
