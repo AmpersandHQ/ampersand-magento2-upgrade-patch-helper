b88b327267c398013cdb0bd8c4e6bc3234ae2393
Luke Rodgers <lr@amp.co>
2021-03-23 16:02:40 +0000

    Safer handling of polyfilled functions

--- src/functions.php
+++ src/functions.php
@@ -7,38 +7,53 @@ if (!shell_exec('which patch')) {
     throw new \Exception('the `patch` command is missing (http://manpages.ubuntu.com/manpages/bionic/man1/patch.1.html)');
 }
 
-/**
- * Returns true only if $string contains $contains
- *
- * @param $string
- * @param $contains
- * @return bool
- */
-function str_contains($string, $contains)
-{
-    return strpos($string, $contains) !== false;
+<?php
+
+if (!shell_exec('which find')) {
+    throw new \Exception('the `find` command is missing (https://ss64.com/bash/find.html)');
+}
+if (!shell_exec('which patch')) {
+    throw new \Exception('the `patch` command is missing (http://manpages.ubuntu.com/manpages/bionic/man1/patch.1.html)');
 }
 
-/**
- * Returns true only if $string starts with $startsWith
- *
- * @param $string
- * @param $startsWith
- * @return bool
- */
-function str_starts_with($string, $startsWith)
-{
-    return substr($string, 0, strlen($startsWith)) === $startsWith;
+if (!function_exists('str_contains')) {
+    /**
+     * Returns true only if $string contains $contains
+     *
+     * @param $string
+     * @param $contains
+     * @return bool
+     */
+    function str_contains($string, $contains)
+    {
+        return strpos($string, $contains) !== false;
+    }
 }
 
-/**
- * Returns true only if $string ends with $endsWith
- *
- * @param $string
- * @param $endsWith
- * @return bool
- */
-function str_ends_with($string, $endsWith)
-{
-    return strlen($endsWith) == 0 || substr($string, -strlen($endsWith)) === $endsWith;
+if (!function_exists('str_starts_with')) {
+    /**
+     * Returns true only if $string starts with $startsWith
+     *
+     * @param $string
+     * @param $startsWith
+     * @return bool
+     */
+    function str_starts_with($string, $startsWith)
+    {
+        return substr($string, 0, strlen($startsWith)) === $startsWith;
+    }
+}
+
+if (!function_exists('str_ends_with')) {
+    /**
+     * Returns true only if $string ends with $endsWith
+     *
+     * @param $string
+     * @param $endsWith
+     * @return bool
+     */
+    function str_ends_with($string, $endsWith)
+    {
+        return strlen($endsWith) == 0 || substr($string, -strlen($endsWith)) === $endsWith;
+    }
 }
