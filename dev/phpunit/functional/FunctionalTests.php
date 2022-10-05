e7bd6b328121d45f7632d2b7b8fca01d30ec1c8e
Luke Rodgers <lukerodgers90@gmail.com>
2022-10-05 14:11:21 +0100

    threeway diff with phpstorm

--- dev/phpunit/functional/FunctionalTests.php
+++ dev/phpunit/functional/FunctionalTests.php
@@ -223,7 +223,7 @@ class FunctionalTests extends \PHPUnit\Framework\TestCase
         $this->assertFileExists(BASE_DIR . '/dev/instances/magentom24nodb/app/etc/di.xml', "Magento 2.4 directory is wrong");
         $this->assertFileDoesNotExist(BASE_DIR . '/dev/instances/magentom24nodb/app/etc/env.php', "Magento 2.4 is installed when it shouldnt be");
 
-        exec($this->generateAnalyseCommand('/dev/instances/../instances/magentom24nodb', '--output-threeway-diff-commands --sort-by-type --vendor-namespaces Ampersand'), $output, $return);
+        exec($this->generateAnalyseCommand('/dev/instances/../instances/magentom24nodb', '--output-threeway-diff-commands=phpstorm.sh --sort-by-type --vendor-namespaces Ampersand'), $output, $return);
         $this->assertEquals(0, $return, "The return code of the command was not zero");
 
         $lastLine = array_pop($output);
