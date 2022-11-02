36e495d20d462fa190fabcd2cf4cbb6fd07e6e63
Luke Rodgers <lr@amp.co>
2022-11-02 19:48:43 +0000

    Add extra error handling to db schema
    I can only test against magento core, we're now doing analysis on a lot of db_schema.xml from a lot of random modules.
    
    I dont want this to break the tool in these cases, if we can't read the file for some reason, suppress it and move on.
    
    We'll see the message in the `-vvv` output

--- src/Ampersand/PatchHelper/Helper/PatchOverrideValidator.php
+++ src/Ampersand/PatchHelper/Helper/PatchOverrideValidator.php
@@ -656,91 +656,95 @@ class PatchOverrideValidator
             return;
         }
 
-        $orginalDefinitions = $this->patchEntry->getDatabaseTablesDefinitionsFromOriginalFile();
-        $newDefinitions = $this->patchEntry->getDatabaseTablesDefinitionsFromNewFile();
+        try {
+            $originalDefinitions = $this->patchEntry->getDatabaseTablesDefinitionsFromOriginalFile();
+            $newDefinitions = $this->patchEntry->getDatabaseTablesDefinitionsFromNewFile();
 
-        foreach ($orginalDefinitions as $tableName => $definition) {
-            if (!isset($newDefinitions[$tableName])) {
-                $this->infos[self::TYPE_DB_SCHEMA_REMOVED][$tableName] = $tableName;
+            foreach ($originalDefinitions as $tableName => $definition) {
+                if (!isset($newDefinitions[$tableName])) {
+                    $this->infos[self::TYPE_DB_SCHEMA_REMOVED][$tableName] = $tableName;
+                }
             }
-        }
-        unset($tableName, $definition);
+            unset($tableName, $definition);
 
-        foreach ($newDefinitions as $tableName => $definition) {
-            if (!isset($orginalDefinitions[$tableName])) {
-                $this->infos[self::TYPE_DB_SCHEMA_ADDED][$tableName] = $tableName;
+            foreach ($newDefinitions as $tableName => $definition) {
+                if (!isset($originalDefinitions[$tableName])) {
+                    $this->infos[self::TYPE_DB_SCHEMA_ADDED][$tableName] = $tableName;
+                }
             }
-        }
-        unset($tableName, $definition);
+            unset($tableName, $definition);
 
-        foreach ($newDefinitions as $tableName => $newDefinition) {
-            if (!(isset($orginalDefinitions[$tableName]) && isset($newDefinitions[$tableName]))) {
-                continue; // This table is not defined in the original and new definitions
+            foreach ($newDefinitions as $tableName => $newDefinition) {
+                if (!(isset($originalDefinitions[$tableName]) && isset($newDefinitions[$tableName]))) {
+                    continue; // This table is not defined in the original and new definitions
+                }
+                if ($originalDefinitions[$tableName]['amp_upgrade_hash'] === $newDefinition['amp_upgrade_hash']) {
+                    continue; // The hash for this table
+                }
+                $this->infos[self::TYPE_DB_SCHEMA_CHANGED][$tableName] = $tableName;
             }
-            if ($orginalDefinitions[$tableName]['amp_upgrade_hash'] === $newDefinition['amp_upgrade_hash']) {
-                continue; // The hash for this table
-            }
-            $this->infos[self::TYPE_DB_SCHEMA_CHANGED][$tableName] = $tableName;
-        }
-        unset($tableName, $definition);
+            unset($tableName, $definition);
 
-        if (
-            empty($this->infos[self::TYPE_DB_SCHEMA_CHANGED]) &&
-            empty($this->infos[self::TYPE_DB_SCHEMA_ADDED]) &&
-            empty($this->infos[self::TYPE_DB_SCHEMA_REMOVED])) {
-            throw new \InvalidArgumentException("$vendorFile could not work out db schema changes for this diff");
-        }
-
-        /*
-         * Promote INFO to WARNING in the case that we are modifying a table defined by another db_schema.xml
-         *
-         * This is identified by looking for the primary key definition of a table, if there's only one we can be
-         * certain that we're modifying a table defined elsewhere
-         *
-         * This ignores magento<->magento modifications, all things are still reported as INFO level otherwise
-         */
-        $primaryTableToFile = $this->m2->getDbSchemaPrimaryDefinition();
-        $primaryDefinitionsInThisFile = [];
-        foreach (self::$dbSchemaTypes as $dbSchemaType) {
-            if (!(isset($this->infos[$dbSchemaType]) && !empty($this->infos[$dbSchemaType]))) {
-                continue;
+            if (
+                empty($this->infos[self::TYPE_DB_SCHEMA_CHANGED]) &&
+                empty($this->infos[self::TYPE_DB_SCHEMA_ADDED]) &&
+                empty($this->infos[self::TYPE_DB_SCHEMA_REMOVED])) {
+                throw new \InvalidArgumentException("$vendorFile could not work out db schema changes for this diff");
             }
-            foreach ($this->infos[$dbSchemaType] as $tableName) {
-                if (!isset($primaryTableToFile[$tableName])) {
+
+            /*
+             * Promote INFO to WARNING in the case that we are modifying a table defined by another db_schema.xml
+             *
+             * This is identified by looking for the primary key definition of a table, if there's only one we can be
+             * certain that we're modifying a table defined elsewhere
+             *
+             * This ignores magento<->magento modifications, all things are still reported as INFO level otherwise
+             */
+            $primaryTableToFile = $this->m2->getDbSchemaPrimaryDefinition();
+            $primaryDefinitionsInThisFile = [];
+            foreach (self::$dbSchemaTypes as $dbSchemaType) {
+                if (!(isset($this->infos[$dbSchemaType]) && !empty($this->infos[$dbSchemaType]))) {
                     continue;
                 }
-                if ($primaryTableToFile[$tableName] === $this->vendorFilepath) {
-                    $primaryDefinitionsInThisFile[$tableName] = $tableName;
+                foreach ($this->infos[$dbSchemaType] as $tableName) {
+                    if (!isset($primaryTableToFile[$tableName])) {
+                        continue;
+                    }
+                    if ($primaryTableToFile[$tableName] === $this->vendorFilepath) {
+                        $primaryDefinitionsInThisFile[$tableName] = $tableName;
+                    }
+                    if ($primaryTableToFile[$tableName] !== $this->vendorFilepath && !str_starts_with($this->vendorFilepath, 'vendor/magento/')) {
+                        $this->warnings[$dbSchemaType][$tableName] = $tableName;
+                        unset($this->infos[$dbSchemaType][$tableName]);
+                    }
                 }
-                if ($primaryTableToFile[$tableName] !== $this->vendorFilepath && !str_starts_with($this->vendorFilepath, 'vendor/magento/')) {
-                    $this->warnings[$dbSchemaType][$tableName] = $tableName;
-                    unset($this->infos[$dbSchemaType][$tableName]);
+            }
+            unset($dbSchemaType, $tableName);
+
+            /*
+             * Flag if a base table definition changes, when there are third party db_schema.xml modifying that table
+             *
+             * Just in case you have some db_schema change in a custom module to fix something in the core
+             *
+             * It may no longer be necessary, or may need tweaked.
+             */
+            if (empty($primaryDefinitionsInThisFile)) {
+                return;
+            }
+
+            $dbSchemaAlterations = $this->m2->getDbSchemaThirdPartyAlteration();
+            foreach ($primaryDefinitionsInThisFile as $primaryTableBeingModified) {
+                if (!isset($dbSchemaAlterations[$primaryTableBeingModified])) {
+                    continue;
+                }
+                foreach ($dbSchemaAlterations[$primaryTableBeingModified] as $thirdPartyDbSchemaModifyingTable) {
+                    $this->warnings[self::TYPE_DB_SCHEMA_TARGET_CHANGED][] = "$thirdPartyDbSchemaModifyingTable ($primaryTableBeingModified)";
                 }
             }
+            unset($primaryTableBeingModified, $thirdPartyDbSchemaModifyingTable);
+        } catch (\Throwable $throwable) {
+            throw new \InvalidArgumentException('db_schema.xml not parseable: ' . $throwable->getMessage());
         }
-        unset($dbSchemaType, $tableName);
-
-        /*
-         * Flag if a base table definition changes, when there are third party db_schema.xml modifying that table
-         *
-         * Just in case you have some db_schema change in a custom module to fix something in the core
-         *
-         * It may no longer be necessary, or may need tweaked.
-         */
-        if (empty($primaryDefinitionsInThisFile)) {
-            return;
-        }
-
-        $dbSchemaAlterations = $this->m2->getDbSchemaThirdPartyAlteration();
-        foreach ($primaryDefinitionsInThisFile as $primaryTableBeingModified) {
-            if (!isset($dbSchemaAlterations[$primaryTableBeingModified])) {
-                continue;
-            }
-            foreach ($dbSchemaAlterations[$primaryTableBeingModified] as $thirdPartyDbSchemaModifyingTable) {
-                $this->warnings[self::TYPE_DB_SCHEMA_TARGET_CHANGED][] = "$thirdPartyDbSchemaModifyingTable ($primaryTableBeingModified)";
-            }
-        }
-        unset($primaryTableBeingModified, $thirdPartyDbSchemaModifyingTable);
     }
 
     /**
