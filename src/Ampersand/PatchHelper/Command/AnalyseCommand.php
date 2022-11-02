3698f2bbf23c6ff7706247c375bba86d6dbeb97c
Luke Rodgers <lr@amp.co>
2022-11-02 17:49:18 +0000

    Update tests

--- src/Ampersand/PatchHelper/Command/AnalyseCommand.php
+++ src/Ampersand/PatchHelper/Command/AnalyseCommand.php
@@ -95,12 +95,6 @@ class AnalyseCommand extends Command
         }
         foreach ($patchFiles as $patchFile) {
             $file = $patchFile->getPath();
-            if (str_contains($file, 'something.phtml')) {
-                continue;
-            }
-            if (str_contains($file, 'grid.phtml')){
-                continue;
-            }
             try {
                 $patchOverrideValidator = new PatchOverrideValidator($magento2, $patchFile);
                 if (!$patchOverrideValidator->canValidate()) {
