6fdbe57e3e15a8432eec5942d02da7487ae91ba2
Luke Rodgers <lr@amp.co>
2023-08-04 10:44:30 +0100

    Test https://github.com/AmpersandHQ/ampersand-magento2-upgrade-patch-helper/issues/106

--- dev/phpunit/unit/resources/checks/ClassPreferencePhp/vendor/magento/module-weee/Model/Total/Quote/Weee.php
+++ dev/phpunit/unit/resources/checks/ClassPreferencePhp/vendor/magento/module-weee/Model/Total/Quote/Weee.php
@@ -1,1 +1,10 @@
 <?php
+namespace Magento\Weee\Model\Total\Quote;
+
+class Weee
+{
+    public function __construct($someNewParam = 123)
+    {
+        // do stuff
+    }
+}
--- dev/phpunit/unit/resources/checks/ClassPreferencePhp/vendor_orig/magento/module-weee/Model/Total/Quote/Weee.php
+++ dev/phpunit/unit/resources/checks/ClassPreferencePhp/vendor_orig/magento/module-weee/Model/Total/Quote/Weee.php
@@ -1,1 +1,10 @@
 <?php
+namespace Magento\Weee\Model\Total\Quote;
+
+class Weee
+{
+    public function __construct()
+    {
+        // do stuff
+    }
+}
